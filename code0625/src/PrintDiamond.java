ab9ca851f46ee872de439365f2bd0393146d7e94
Hsuehlei <40349760+Hsuehlei@users.noreply.github.com>
2020-06-27 09:53:06 +0800

    Update PrintDiamond.java

--- code0625/src/PrintDiamond.java
+++ code0625/src/PrintDiamond.java
@@ -7,24 +7,29 @@
 public class PrintDiamond {
     public static void main(String[] args) {
 
-        int size=4;
-        for (int i = 1; i <= size; i++) {
-            for (int j = 1; j <= size - i; j++) {
+        //打印菱形分两段
+        int rowNumUp = 5;
+        //上五行
+        for (int i = 1; i <= rowNumUp; i++) {
+
+            for (int j = 1; j <= rowNumUp - i; j++) {
                 System.out.print(" ");
             }
 
-            for (int k = 1; k <= 2 * i - 1; k++) {
-                System.out.print('*');
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
-        for (int i = 1; i <= size-1; i++) {
-            for (int j = 1; j <= i; j++){
+
+        //下四行
+        int rowNumDown = 4;
+        for (int i = 1; i <=rowNumDown; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print(" ");
             }
-
-            for (int k = 2*size-3; k >= 2 * i - 1; k--){
-                System.out.print('*');
+            for (int j = 1; j <= (2*(4 - i + 1)-1) ; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
