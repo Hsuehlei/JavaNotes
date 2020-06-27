5dea5a102c6c0dce40ea7672894648d567151b94
Hsuehlei <40349760+Hsuehlei@users.noreply.github.com>
2020-06-27 09:55:57 +0800

    Update Since1990.java

--- code0625/src/Since1990.java
+++ code0625/src/Since1990.java
@@ -9,66 +9,44 @@ import java.util.Scanner;
 public class Since1990 {
     public static void main(String[] args) {
 
-        int first = 1900;
-        int leapYearTimes = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.println("请输入日期（如：2000）：");
         int year = sc.nextInt();
-        System.out.println("请输入日期（如：05）：");
         int month = sc.nextInt();
-        System.out.println("请输入日期（如：05）：");
         int day = sc.nextInt();
 
-        for (int i = first; i <= year; i++) {
-            if (year % 4 == 0 && year % 100 != 0) {
-                leapYearTimes++;
+        int sumDay = day - 1;
+
+        //年相差
+        for (int i = 1900; i < year; i++) {
+            if (((i % 4 == 0) && (i % 100 != 0)) || i % 400 == 0) {
+                sumDay = sumDay + 366;
+            } else {
+                sumDay = sumDay + 365;
             }
         }
 
-        int yearDay = (year - 1900) * 365 + leapYearTimes;
-        int monthDay;
-        int dayDay = day - 1;
-
-        switch (month) {
-            case 1:
-                monthDay = 0;
-                break;
-            case 2:
-                monthDay = 31;
-                break;
-            case 3:
-                monthDay = 31 + 28;
-                break;
-            case 4:
-                monthDay = 31 + 28 + 31;
-                break;
-            case 5:
-                monthDay = 31 + 28 + 31 + 30;
-                break;
-            case 6:
-                monthDay = 31 + 28 + 31 + 30 + 31;
-                break;
-            case 7:
-                monthDay = 31 + 28 + 31 + 30 + 31 + 30;
-                break;
-            case 8:
-                monthDay = 31 + 28 + 31 + 30 + 31 + 30 + 31;
-                break;
-            case 9:
-                monthDay = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-                break;
-            case 10:
-                monthDay = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-                break;
-            case 11:
-                monthDay = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-                break;
-            default:
-                monthDay = 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
+        for (int i = 1; i < month; i++) {
+            switch (i) {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    sumDay += 31;
+                    break;
+                case 2:
+                    if (((i % 4 == 0) && (i % 100 != 0)) || i % 400 == 0) {
+                        sumDay = sumDay + 29;
+                    } else {
+                        sumDay = sumDay + 28;
+                    }
+                    break;
+                default:
+                    sumDay += 30;
+            }
         }
-
-        int total = yearDay + monthDay + dayDay;
-
-        System.out.println("1900年1月1日距输入的天数大概" + total + "天");
+        System.out.println(sumDay);
     }
 }
