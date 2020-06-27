cb33e754b25da0f6e9f5d8db8e41767d38091281
Hsuehlei <40349760+Hsuehlei@users.noreply.github.com>
2020-06-27 09:50:43 +0800

    Update PrintTriangle.java

--- code0625/src/PrintTriangle.java
+++ code0625/src/PrintTriangle.java
@@ -2,25 +2,23 @@
  * @author Lei
  * @version 1.0
  * @date 2020/6/25 19:12
- * @description 打印三角形
+ * @description 打印等腰三角形
  */
 public class PrintTriangle {
     public static void main(String[] args) {
 
-        for(int i=1;i<=4;i++){
-
-            for(int j=3; i<=j; j--) {
+        int rowNum = 4;
+        for (int i = 1; i <= rowNum; i++) {
+            //打印空格和打印星号都是循环再做
+            //打印空格
+            for (int j = 1; j <= rowNum - i; j++) {
                 System.out.print(" ");
             }
 
-            for(int j=1; j<=i; j++) {
+            //打印星号
+            for (int j = 1; j <= (i * 2 - 1); j++) {
                 System.out.print("*");
             }
-
-            for(int j=1; j<i; j++) {
-                System.out.print("*");
-            }
-
             System.out.println();
         }
     }
