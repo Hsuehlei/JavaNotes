3ef18f354b292ec0583a671e9aadd7c13267443b
Hsuehlei <40349760+Hsuehlei@users.noreply.github.com>
2020-06-27 09:38:05 +0800

    Update and rename MonkeyPeach.java to Peach.java

--- code0625/src/Peach.java
+++ code0625/src/Peach.java
@@ -6,7 +6,7 @@
  *              以后每天早上都吃了前一天剩下的一半零一个。
  *              到第十天早上想再吃时，见只剩一个桃子了。求第一天共摘了多少？
  */
-public class MonkeyPeach {
+public class Peach {
     public static void main(String[] args) {
 
         int peachNum = 1;
